bfcbba3723b7bc6d1a70c30886c10debde05b24c
laaptu <sanimap@gmail.com>
2018-03-11 20:01:20 +1100

    Adding version1.0 apk

--- app/src/main/java/com/countryfacts/network/Urls.java
+++
@@ -1,9 +0,0 @@
-package com.countryfacts.network;
-
-/**
- */
-
-public class Urls {
-    public static String BASE = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/";
-    public static final String FACTS = "facts.json";
-}
